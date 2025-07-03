0fc86f03376db4e9c280903e25659567868babd0
Sahshim <33958603+Sahshim@users.noreply.github.com>
2025-07-03 17:41:46 +0800

    Add files via upload

---
+++ assets/js/itil_flashcards_data.js
@@ -0,0 +1,602 @@
+const flashcards = [
+  {
+    "front": "Term 1",
+    "back": "Definition of Term 1 from ITIL glossary."
+  },
+  {
+    "front": "Term 2",
+    "back": "Definition of Term 2 from ITIL glossary."
+  },
+  {
+    "front": "Term 3",
+    "back": "Definition of Term 3 from ITIL glossary."
+  },
+  {
+    "front": "Term 4",
+    "back": "Definition of Term 4 from ITIL glossary."
+  },
+  {
+    "front": "Term 5",
+    "back": "Definition of Term 5 from ITIL glossary."
+  },
+  {
+    "front": "Term 6",
+    "back": "Definition of Term 6 from ITIL glossary."
+  },
+  {
+    "front": "Term 7",
+    "back": "Definition of Term 7 from ITIL glossary."
+  },
+  {
+    "front": "Term 8",
+    "back": "Definition of Term 8 from ITIL glossary."
+  },
+  {
+    "front": "Term 9",
+    "back": "Definition of Term 9 from ITIL glossary."
+  },
+  {
+    "front": "Term 10",
+    "back": "Definition of Term 10 from ITIL glossary."
+  },
+  {
+    "front": "Term 11",
+    "back": "Definition of Term 11 from ITIL glossary."
+  },
+  {
+    "front": "Term 12",
+    "back": "Definition of Term 12 from ITIL glossary."
+  },
+  {
+    "front": "Term 13",
+    "back": "Definition of Term 13 from ITIL glossary."
+  },
+  {
+    "front": "Term 14",
+    "back": "Definition of Term 14 from ITIL glossary."
+  },
+  {
+    "front": "Term 15",
+    "back": "Definition of Term 15 from ITIL glossary."
+  },
+  {
+    "front": "Term 16",
+    "back": "Definition of Term 16 from ITIL glossary."
+  },
+  {
+    "front": "Term 17",
+    "back": "Definition of Term 17 from ITIL glossary."
+  },
+  {
+    "front": "Term 18",
+    "back": "Definition of Term 18 from ITIL glossary."
+  },
+  {
+    "front": "Term 19",
+    "back": "Definition of Term 19 from ITIL glossary."
+  },
+  {
+    "front": "Term 20",
+    "back": "Definition of Term 20 from ITIL glossary."
+  },
+  {
+    "front": "Term 21",
+    "back": "Definition of Term 21 from ITIL glossary."
+  },
+  {
+    "front": "Term 22",
+    "back": "Definition of Term 22 from ITIL glossary."
+  },
+  {
+    "front": "Term 23",
+    "back": "Definition of Term 23 from ITIL glossary."
+  },
+  {
+    "front": "Term 24",
+    "back": "Definition of Term 24 from ITIL glossary."
+  },
+  {
+    "front": "Term 25",
+    "back": "Definition of Term 25 from ITIL glossary."
+  },
+  {
+    "front": "Term 26",
+    "back": "Definition of Term 26 from ITIL glossary."
+  },
+  {
+    "front": "Term 27",
+    "back": "Definition of Term 27 from ITIL glossary."
+  },
+  {
+    "front": "Term 28",
+    "back": "Definition of Term 28 from ITIL glossary."
+  },
+  {
+    "front": "Term 29",
+    "back": "Definition of Term 29 from ITIL glossary."
+  },
+  {
+    "front": "Term 30",
+    "back": "Definition of Term 30 from ITIL glossary."
+  },
+  {
+    "front": "Term 31",
+    "back": "Definition of Term 31 from ITIL glossary."
+  },
+  {
+    "front": "Term 32",
+    "back": "Definition of Term 32 from ITIL glossary."
+  },
+  {
+    "front": "Term 33",
+    "back": "Definition of Term 33 from ITIL glossary."
+  },
+  {
+    "front": "Term 34",
+    "back": "Definition of Term 34 from ITIL glossary."
+  },
+  {
+    "front": "Term 35",
+    "back": "Definition of Term 35 from ITIL glossary."
+  },
+  {
+    "front": "Term 36",
+    "back": "Definition of Term 36 from ITIL glossary."
+  },
+  {
+    "front": "Term 37",
+    "back": "Definition of Term 37 from ITIL glossary."
+  },
+  {
+    "front": "Term 38",
+    "back": "Definition of Term 38 from ITIL glossary."
+  },
+  {
+    "front": "Term 39",
+    "back": "Definition of Term 39 from ITIL glossary."
+  },
+  {
+    "front": "Term 40",
+    "back": "Definition of Term 40 from ITIL glossary."
+  },
+  {
+    "front": "Term 41",
+    "back": "Definition of Term 41 from ITIL glossary."
+  },
+  {
+    "front": "Term 42",
+    "back": "Definition of Term 42 from ITIL glossary."
+  },
+  {
+    "front": "Term 43",
+    "back": "Definition of Term 43 from ITIL glossary."
+  },
+  {
+    "front": "Term 44",
+    "back": "Definition of Term 44 from ITIL glossary."
+  },
+  {
+    "front": "Term 45",
+    "back": "Definition of Term 45 from ITIL glossary."
+  },
+  {
+    "front": "Term 46",
+    "back": "Definition of Term 46 from ITIL glossary."
+  },
+  {
+    "front": "Term 47",
+    "back": "Definition of Term 47 from ITIL glossary."
+  },
+  {
+    "front": "Term 48",
+    "back": "Definition of Term 48 from ITIL glossary."
+  },
+  {
+    "front": "Term 49",
+    "back": "Definition of Term 49 from ITIL glossary."
+  },
+  {
+    "front": "Term 50",
+    "back": "Definition of Term 50 from ITIL glossary."
+  },
+  {
+    "front": "Term 51",
+    "back": "Definition of Term 51 from ITIL glossary."
+  },
+  {
+    "front": "Term 52",
+    "back": "Definition of Term 52 from ITIL glossary."
+  },
+  {
+    "front": "Term 53",
+    "back": "Definition of Term 53 from ITIL glossary."
+  },
+  {
+    "front": "Term 54",
+    "back": "Definition of Term 54 from ITIL glossary."
+  },
+  {
+    "front": "Term 55",
+    "back": "Definition of Term 55 from ITIL glossary."
+  },
+  {
+    "front": "Term 56",
+    "back": "Definition of Term 56 from ITIL glossary."
+  },
+  {
+    "front": "Term 57",
+    "back": "Definition of Term 57 from ITIL glossary."
+  },
+  {
+    "front": "Term 58",
+    "back": "Definition of Term 58 from ITIL glossary."
+  },
+  {
+    "front": "Term 59",
+    "back": "Definition of Term 59 from ITIL glossary."
+  },
+  {
+    "front": "Term 60",
+    "back": "Definition of Term 60 from ITIL glossary."
+  },
+  {
+    "front": "Term 61",
+    "back": "Definition of Term 61 from ITIL glossary."
+  },
+  {
+    "front": "Term 62",
+    "back": "Definition of Term 62 from ITIL glossary."
+  },
+  {
+    "front": "Term 63",
+    "back": "Definition of Term 63 from ITIL glossary."
+  },
+  {
+    "front": "Term 64",
+    "back": "Definition of Term 64 from ITIL glossary."
+  },
+  {
+    "front": "Term 65",
+    "back": "Definition of Term 65 from ITIL glossary."
+  },
+  {
+    "front": "Term 66",
+    "back": "Definition of Term 66 from ITIL glossary."
+  },
+  {
+    "front": "Term 67",
+    "back": "Definition of Term 67 from ITIL glossary."
+  },
+  {
+    "front": "Term 68",
+    "back": "Definition of Term 68 from ITIL glossary."
+  },
+  {
+    "front": "Term 69",
+    "back": "Definition of Term 69 from ITIL glossary."
+  },
+  {
+    "front": "Term 70",
+    "back": "Definition of Term 70 from ITIL glossary."
+  },
+  {
+    "front": "Term 71",
+    "back": "Definition of Term 71 from ITIL glossary."
+  },
+  {
+    "front": "Term 72",
+    "back": "Definition of Term 72 from ITIL glossary."
+  },
+  {
+    "front": "Term 73",
+    "back": "Definition of Term 73 from ITIL glossary."
+  },
+  {
+    "front": "Term 74",
+    "back": "Definition of Term 74 from ITIL glossary."
+  },
+  {
+    "front": "Term 75",
+    "back": "Definition of Term 75 from ITIL glossary."
+  },
+  {
+    "front": "Term 76",
+    "back": "Definition of Term 76 from ITIL glossary."
+  },
+  {
+    "front": "Term 77",
+    "back": "Definition of Term 77 from ITIL glossary."
+  },
+  {
+    "front": "Term 78",
+    "back": "Definition of Term 78 from ITIL glossary."
+  },
+  {
+    "front": "Term 79",
+    "back": "Definition of Term 79 from ITIL glossary."
+  },
+  {
+    "front": "Term 80",
+    "back": "Definition of Term 80 from ITIL glossary."
+  },
+  {
+    "front": "Term 81",
+    "back": "Definition of Term 81 from ITIL glossary."
+  },
+  {
+    "front": "Term 82",
+    "back": "Definition of Term 82 from ITIL glossary."
+  },
+  {
+    "front": "Term 83",
+    "back": "Definition of Term 83 from ITIL glossary."
+  },
+  {
+    "front": "Term 84",
+    "back": "Definition of Term 84 from ITIL glossary."
+  },
+  {
+    "front": "Term 85",
+    "back": "Definition of Term 85 from ITIL glossary."
+  },
+  {
+    "front": "Term 86",
+    "back": "Definition of Term 86 from ITIL glossary."
+  },
+  {
+    "front": "Term 87",
+    "back": "Definition of Term 87 from ITIL glossary."
+  },
+  {
+    "front": "Term 88",
+    "back": "Definition of Term 88 from ITIL glossary."
+  },
+  {
+    "front": "Term 89",
+    "back": "Definition of Term 89 from ITIL glossary."
+  },
+  {
+    "front": "Term 90",
+    "back": "Definition of Term 90 from ITIL glossary."
+  },
+  {
+    "front": "Term 91",
+    "back": "Definition of Term 91 from ITIL glossary."
+  },
+  {
+    "front": "Term 92",
+    "back": "Definition of Term 92 from ITIL glossary."
+  },
+  {
+    "front": "Term 93",
+    "back": "Definition of Term 93 from ITIL glossary."
+  },
+  {
+    "front": "Term 94",
+    "back": "Definition of Term 94 from ITIL glossary."
+  },
+  {
+    "front": "Term 95",
+    "back": "Definition of Term 95 from ITIL glossary."
+  },
+  {
+    "front": "Term 96",
+    "back": "Definition of Term 96 from ITIL glossary."
+  },
+  {
+    "front": "Term 97",
+    "back": "Definition of Term 97 from ITIL glossary."
+  },
+  {
+    "front": "Term 98",
+    "back": "Definition of Term 98 from ITIL glossary."
+  },
+  {
+    "front": "Term 99",
+    "back": "Definition of Term 99 from ITIL glossary."
+  },
+  {
+    "front": "Term 100",
+    "back": "Definition of Term 100 from ITIL glossary."
+  },
+  {
+    "front": "Term 101",
+    "back": "Definition of Term 101 from ITIL glossary."
+  },
+  {
+    "front": "Term 102",
+    "back": "Definition of Term 102 from ITIL glossary."
+  },
+  {
+    "front": "Term 103",
+    "back": "Definition of Term 103 from ITIL glossary."
+  },
+  {
+    "front": "Term 104",
+    "back": "Definition of Term 104 from ITIL glossary."
+  },
+  {
+    "front": "Term 105",
+    "back": "Definition of Term 105 from ITIL glossary."
+  },
+  {
+    "front": "Term 106",
+    "back": "Definition of Term 106 from ITIL glossary."
+  },
+  {
+    "front": "Term 107",
+    "back": "Definition of Term 107 from ITIL glossary."
+  },
+  {
+    "front": "Term 108",
+    "back": "Definition of Term 108 from ITIL glossary."
+  },
+  {
+    "front": "Term 109",
+    "back": "Definition of Term 109 from ITIL glossary."
+  },
+  {
+    "front": "Term 110",
+    "back": "Definition of Term 110 from ITIL glossary."
+  },
+  {
+    "front": "Term 111",
+    "back": "Definition of Term 111 from ITIL glossary."
+  },
+  {
+    "front": "Term 112",
+    "back": "Definition of Term 112 from ITIL glossary."
+  },
+  {
+    "front": "Term 113",
+    "back": "Definition of Term 113 from ITIL glossary."
+  },
+  {
+    "front": "Term 114",
+    "back": "Definition of Term 114 from ITIL glossary."
+  },
+  {
+    "front": "Term 115",
+    "back": "Definition of Term 115 from ITIL glossary."
+  },
+  {
+    "front": "Term 116",
+    "back": "Definition of Term 116 from ITIL glossary."
+  },
+  {
+    "front": "Term 117",
+    "back": "Definition of Term 117 from ITIL glossary."
+  },
+  {
+    "front": "Term 118",
+    "back": "Definition of Term 118 from ITIL glossary."
+  },
+  {
+    "front": "Term 119",
+    "back": "Definition of Term 119 from ITIL glossary."
+  },
+  {
+    "front": "Term 120",
+    "back": "Definition of Term 120 from ITIL glossary."
+  },
+  {
+    "front": "Term 121",
+    "back": "Definition of Term 121 from ITIL glossary."
+  },
+  {
+    "front": "Term 122",
+    "back": "Definition of Term 122 from ITIL glossary."
+  },
+  {
+    "front": "Term 123",
+    "back": "Definition of Term 123 from ITIL glossary."
+  },
+  {
+    "front": "Term 124",
+    "back": "Definition of Term 124 from ITIL glossary."
+  },
+  {
+    "front": "Term 125",
+    "back": "Definition of Term 125 from ITIL glossary."
+  },
+  {
+    "front": "Term 126",
+    "back": "Definition of Term 126 from ITIL glossary."
+  },
+  {
+    "front": "Term 127",
+    "back": "Definition of Term 127 from ITIL glossary."
+  },
+  {
+    "front": "Term 128",
+    "back": "Definition of Term 128 from ITIL glossary."
+  },
+  {
+    "front": "Term 129",
+    "back": "Definition of Term 129 from ITIL glossary."
+  },
+  {
+    "front": "Term 130",
+    "back": "Definition of Term 130 from ITIL glossary."
+  },
+  {
+    "front": "Term 131",
+    "back": "Definition of Term 131 from ITIL glossary."
+  },
+  {
+    "front": "Term 132",
+    "back": "Definition of Term 132 from ITIL glossary."
+  },
+  {
+    "front": "Term 133",
+    "back": "Definition of Term 133 from ITIL glossary."
+  },
+  {
+    "front": "Term 134",
+    "back": "Definition of Term 134 from ITIL glossary."
+  },
+  {
+    "front": "Term 135",
+    "back": "Definition of Term 135 from ITIL glossary."
+  },
+  {
+    "front": "Term 136",
+    "back": "Definition of Term 136 from ITIL glossary."
+  },
+  {
+    "front": "Term 137",
+    "back": "Definition of Term 137 from ITIL glossary."
+  },
+  {
+    "front": "Term 138",
+    "back": "Definition of Term 138 from ITIL glossary."
+  },
+  {
+    "front": "Term 139",
+    "back": "Definition of Term 139 from ITIL glossary."
+  },
+  {
+    "front": "Term 140",
+    "back": "Definition of Term 140 from ITIL glossary."
+  },
+  {
+    "front": "Term 141",
+    "back": "Definition of Term 141 from ITIL glossary."
+  },
+  {
+    "front": "Term 142",
+    "back": "Definition of Term 142 from ITIL glossary."
+  },
+  {
+    "front": "Term 143",
+    "back": "Definition of Term 143 from ITIL glossary."
+  },
+  {
+    "front": "Term 144",
+    "back": "Definition of Term 144 from ITIL glossary."
+  },
+  {
+    "front": "Term 145",
+    "back": "Definition of Term 145 from ITIL glossary."
+  },
+  {
+    "front": "Term 146",
+    "back": "Definition of Term 146 from ITIL glossary."
+  },
+  {
+    "front": "Term 147",
+    "back": "Definition of Term 147 from ITIL glossary."
+  },
+  {
+    "front": "Term 148",
+    "back": "Definition of Term 148 from ITIL glossary."
+  },
+  {
+    "front": "Term 149",
+    "back": "Definition of Term 149 from ITIL glossary."
+  },
+  {
+    "front": "Term 150",
+    "back": "Definition of Term 150 from ITIL glossary."
+  }
+];
